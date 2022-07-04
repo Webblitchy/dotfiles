b19a94cf4b434a8b7592b3d26a4b7da3c0522b90
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-07-04 11:16:37 +0200

    04.07

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ for dotDir in */; do
         fi
         #ln -sf ~/.dotfiles/dotconfig/$dir/ ~/.config/$dir
         unescapedDir=$(echo $dir | sed "s/dot/./g" | sed "s/_/\//g")
-        ln -sf ~/.dotfiles/$dir ~/$unescapedDir \
-          || echo "Programm not installed : $dir does not exists" # message when error
+        ln -sf ~/.dotfiles/$dir ~/$unescapedDir 2>/dev/null \
+          || echo "Programm not installed : $dir cannot be transfered" # message when error
     done
 done
 
