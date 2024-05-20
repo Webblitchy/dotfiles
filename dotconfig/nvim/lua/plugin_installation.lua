eccc955f09de42172e141745d86303f9685a7d2c
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2024-05-20 17:05:27 +0200

    20.05.2024

--- dotconfig/nvim/lua/plugin_installation.lua
+++ dotconfig/nvim/lua/plugin_installation.lua
@@ -240,10 +240,19 @@ require("lazy").setup({
   {
     "mfussenegger/nvim-dap-python",
     ft = "python"
-  }
+  },
 
   -- Remote-nvim
-
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*",                     -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- For standard functions
+      "MunifTanjim/nui.nvim",          -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = true,
+  },
 
 
 }, {
